2e01d92aabcef85a1742d81650c1a54f99450321
doerrer <n.doerrer@uib.de>
2021-01-20 16:17:49 +0100

    [chg] dropped lazy load approach, load plugins as modules via importlib

--- opsicli/__init__.py
+++ opsicli/__init__.py
@@ -1,31 +1,46 @@
 import os
+import importlib
 import click
 
 __version__ = "0.1.0"
 
 #https://click.palletsprojects.com/en/7.x/commands/#custom-multi-commands
-plugin_folder = os.path.join(os.path.dirname(__file__), 'commands')
-
 class OpsiCLI(click.MultiCommand):
-	def list_commands(self, ctx):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.plugin_folders = [os.path.expanduser("~/.local/lib/opsicli/commands")]
+		self.plugin_modules = []
+		self.plugin_info = {}
+
+	def register_commands(self):
 		result = []
-		for filename in os.listdir(plugin_folder):
-			if not os.path.isdir(os.path.join(plugin_folder, filename)):
-				continue
-			if os.path.exists(os.path.join(plugin_folder, filename, "__init__.py")):
-				result.append(filename)
-		result.sort()
-		return result
+		for folder in self.plugin_folders:
+			for filename in os.listdir(folder):
+				path = os.path.join(folder, filename, "__init__.py")
+				if os.path.exists(path):
+					spec = importlib.util.spec_from_file_location("dummy", path)
+					self.plugin_modules.append(importlib.util.module_from_spec(spec))
+					spec.loader.exec_module(self.plugin_modules[-1])
+					info = self.plugin_modules[-1].get_plugin_info()
+					self.plugin_info[info["name"]] = info
+					self.plugin_info[info["name"]]["index"] = len(self.plugin_modules) - 1
+
+					#print(f'Adding command {info["name"]}')
+					result.append(info["name"])
+
+	def list_commands(self, ctx):
+		if not self.plugin_info:
+			self.register_commands()
+		return sorted(self.plugin_info.keys())
 
 	def get_command(self, ctx, cmd_name):
-		if cmd_name == "__init__":
-			return None 	# cli is called without subcommand
-		namespace = {}
-		filename = os.path.join(plugin_folder, cmd_name, '__init__.py')
-		with open(filename) as command_file:
-			code = compile(command_file.read(), filename, 'exec')
-			eval(code, namespace, namespace)		# pylint: disable=W0123
-		return namespace['cli']
+		if not cmd_name in self.plugin_info:
+			self.register_commands()
+			if not cmd_name in self.plugin_info:
+				raise ValueError(f"invalid command {cmd_name}")
+		index = self.plugin_info[cmd_name]["index"]
+		return self.plugin_modules[index].cli
+
 
 @click.command(cls=OpsiCLI)
 #@click.version_option(f"{__version__}", message="%(package)s, version %(version)s")
--- opsicli/commands/support/__init__.py
+++ opsicli/commands/support/__init__.py
@@ -2,12 +2,22 @@ import click
 
 __version__ = "0.1.0"
 
-@click.group()
+def get_plugin_info():
+	return {
+		"name" : "support",
+		"subcommands" : ["ticket"],
+		"help" : "this is the support help"
+	}
+
+def get_plugin_name():
+	return "support"
+
+@click.group(short_help='short help for support')
 #@click.version_option(f"{__version__}", message="%(package)s, version %(version)s")
-@click.version_option(f"{__version__}", message="opsi support, version %(version)s")
+@click.version_option(__version__, message="opsi support, version %(version)s")
 def cli():
 	print("support subcommand")
 
-@cli.command()
+@cli.command(short_help='short help for ticket')
 def ticket():
 	print("ticket subcommand")
