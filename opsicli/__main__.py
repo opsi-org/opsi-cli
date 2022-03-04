0857ec51a5789b0249aaaabc9385128af6ad48a4
Jan Schneider <j.schneider@uib.de>
2022-03-04 11:44:04 +0100

    [chg] Rework logging options

--- opsicli/__main__.py
+++ opsicli/__main__.py
@@ -11,10 +11,18 @@ from types import ModuleType
 from pathlib import Path
 from importlib.util import spec_from_file_location, module_from_spec
 from click.exceptions import ClickException, Abort
+from click.shell_completion import CompletionItem
 import rich_click as click  # type: ignore[import]
 from rich_click.rich_click import rich_format_error, rich_abort_error, rich_format_help  # type: ignore[import]
 
-from opsicommon.logging import logger, logging_config, DEFAULT_FORMAT, DEFAULT_COLORED_FORMAT  # type: ignore[import]
+from opsicommon.logging import (  # type: ignore[import]
+	logger,
+	logging_config,
+	DEFAULT_FORMAT,
+	DEFAULT_COLORED_FORMAT,
+	NAME_TO_LEVEL,
+	LEVEL_TO_OPSI_LEVEL,
+)
 
 from opsicli import plugin, prepare_cli_paths, prepare_context, __version__
 from opsicli.config import config
@@ -26,7 +34,7 @@ click.rich_click.OPTION_GROUPS = {
 	"opsi-cli": [
 		{
 			"name": "General options",
-			"options": ["--version", "--help", "--color", "--log-file", "--log-level"],
+			"options": ["--version", "--help", "--color", "--log-file", "--log-level-file", "--log-level-stderr"],
 		},
 		{
 			"name": "Opsi service options",
@@ -124,6 +132,42 @@ class OpsiCLI(click.MultiCommand):
 		return self.plugin_modules[cmd_name].cli
 
 
+class LogLevel(click.ParamType):
+	name = "log-level"
+	possible_level_names = reversed([v.lower() for v in NAME_TO_LEVEL])
+	possible_values_for_help = ", ".join([f"{name}/{LEVEL_TO_OPSI_LEVEL[NAME_TO_LEVEL[name.upper()]]}" for name in possible_level_names])
+
+	def convert(self, value: Any, param: Optional[click.Parameter], ctx: Optional[click.Context]) -> int:
+		try:
+			value = min(9, max(0, int(value)))
+		except ValueError:
+			try:
+				value = LEVEL_TO_OPSI_LEVEL[NAME_TO_LEVEL[value.upper()]]
+			except KeyError:
+				self.fail(
+					f"{value!r} is not a valid log level, choose on of: {', '.join(self.possible_level_names)}",
+					param,
+					ctx,
+				)
+		return value
+
+	def get_metavar(self, param: click.Parameter) -> str:
+		return "LOG_LEVEL"
+
+	def shell_complete(self, ctx: click.Context, param: click.Parameter, incomplete: str) -> List[CompletionItem]:
+		completion_items = []
+		try:
+			completion_items = [CompletionItem(min(9, max(0, int(incomplete))))]
+		except ValueError:
+			for name in self.possible_level_names:
+				if name.startswith(incomplete.lower()):
+					completion_items.append(name)
+		return completion_items
+
+	def __repr__(self) -> str:
+		return "LOG_LEVEL"
+
+
 @click.command(cls=OpsiCLI)
 @click.pass_context
 @click.version_option(f"{__version__}", message="opsi-cli version %(version)s")
@@ -136,17 +180,19 @@ class OpsiCLI(click.MultiCommand):
 	help="Log to LOG_FILE",
 )
 @click.option(
-	"--log-level",
-	"-l",
-	default=4,
-	type=click.IntRange(min=1, max=9),
-	metavar="LEVEL",
+	"--log-level-file",
+	default="none",
+	type=LogLevel(),
 	show_default=True,
-	help=(
-		"Set the log level for the log file.\n\n"
-		"0: nothing, 1: essential, 2: critical, 3: errors, 4: warnings, "
-		"5: notices, 6: infos, 7: debug, 8: trace, 9: secrets"
-	),
+	help=f"Set the log level for the log file. Possible values are:\n\n{LogLevel.possible_values_for_help}",
+)
+@click.option(
+	"--log-level-stderr",
+	"-l",
+	default="warning",
+	type=LogLevel(),
+	show_default=True,
+	help=f"Set the log level for stderr. Possible values are:\n\n{LogLevel.possible_values_for_help}",
 )
 @click.option(
 	"--color/--no-color",
@@ -159,14 +205,24 @@ class OpsiCLI(click.MultiCommand):
 @click.option("--username", "-u", envvar="OPSI_USERNAME", metavar="USERNAME", type=str)
 @click.option("--password", "-p", envvar="OPSI_PASSWORD", metavar="PASSWORD", type=str)
 def main(  # pylint: disable=too-many-arguments
-	ctx: click.Context, color: bool, log_file: pathlib.Path, log_level: int, service_url: str, username: str, password: str
+	ctx: click.Context,
+	color: bool,
+	log_level_stderr: int,
+	log_file: pathlib.Path,
+	log_level_file: int,
+	service_url: str,
+	username: str,
+	password: str,
 ) -> None:
 	"""
 	opsi command line interface\n
 	Plugins are dynamically loaded from a subfolder
 	"""
 	logging_config(
-		log_file=log_file, file_level=log_level, stderr_level=log_level, stderr_format=DEFAULT_COLORED_FORMAT if color else DEFAULT_FORMAT
+		log_file=log_file,
+		file_level=log_level_file,
+		stderr_level=log_level_stderr,
+		stderr_format=DEFAULT_COLORED_FORMAT if color else DEFAULT_FORMAT,
 	)
 	if not ctx.obj:  # stacked execution in pytest circumvents register_plugins -> explicit call here
 		logger.notice("Explicitely calling register_plugins")
