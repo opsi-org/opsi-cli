bc172fa2af24d0a284377f5b59340cf2269ad4ad
Jan Schneider <j.schneider@uib.de>
2022-04-02 09:58:28 +0200

    [chg] Write service to config file

--- plugins/config/python/__init__.py
+++ plugins/config/python/__init__.py
@@ -10,9 +10,10 @@ from urllib.parse import urlparse
 import rich_click as click  # type: ignore[import]
 from click.shell_completion import CompletionItem
 from opsicommon.logging import logger  # type: ignore[import]
+from ruamel.yaml import YAML
 
 from opsicli.config import config
-from opsicli.io import write_output
+from opsicli.io import prompt, write_output
 from opsicli.plugin import OPSICLIPlugin
 from opsicli.types import OPSIService
 
@@ -91,27 +92,69 @@ def service_list() -> None:
 	"""
 	opsi-cli config service list subcommand.
 	"""
-	print(config.services)
+	metadata = {
+		"attributes": [
+			{"id": "name", "description": "The service identifier", "identifier": True},
+			{"id": "url", "description": "The base url of the opsi service"},
+			{"id": "username", "description": "Username to use for authentication"},
+			{"id": "password", "description": "Password to use for authentication"},
+		]
+	}
+	data = []
+	for item in sorted(config.services, key=lambda x: x.name):
+		data.append({"name": item.name, "url": item.url, "username": item.username, "password": "*****" if item.password else ""})
+
+	write_output(data, metadata)
 
 
 @service.command(name="add", short_help="Add an opsi service")
 @click.argument("url", type=str, required=False)
-@click.argument("name", type=str, required=False)
-def service_add(url: Optional[str] = None, name: Optional[str] = None) -> None:
+# @click.argument("name", type=str, required=False)
+@click.option("--name", type=str, required=False, default=None)
+@click.option("--username", type=str, required=False, default=None)
+@click.option("--password", type=str, required=False, default=None)
+@click.option("--system", type=bool, required=False, default=False)
+def service_add(
+	url: Optional[str] = None,
+	name: Optional[str] = None,
+	username: Optional[str] = None,
+	password: Optional[str] = None,
+	system: bool = False,
+) -> None:
 	"""
 	opsi-cli config service add subcommand.
 	"""
+	interactive = False
 	if not url:
 		if not config.interactive:
 			raise ValueError("No url specified")
-		url = click.prompt("Please enter the base url of the opsi service", type=str, default="https://localhost:4447")
+		interactive = True
+		url = prompt("Please enter the base url of the opsi service", default="https://localhost:4447")
+
 	ourl = urlparse(url)
 	if not name:
 		name = ourl.hostname
-		if config.interactive:
-			name = click.prompt("Please enter a name for the service", type=str, default=name)
+		if interactive:
+			name = prompt("Please enter a name for the service", default=name)
 
-	config.get_config_item("services").add_value(OPSIService(name, url))
+	if not username and interactive:
+		username = prompt("Enter the username to use for authentication (optional)") or None
+
+	if not password and interactive:
+		password = prompt("Enter the password to use for authentication (optional)", password=True) or None
+
+	new_service = OPSIService(name=name, url=url, username=username, password=password)
+
+	data = {}
+	config_file = config.config_file_system if system else config.config_file_user
+	if config_file.exists():
+		with open(config_file, "r", encoding="utf-8") as file:
+			data = YAML().load(file)
+	if "services" not in data:
+		data["services"] = {}
+	data["services"][new_service.name] = {"url": new_service.url}
+	with open(config_file, "w", encoding="utf-8") as file:
+		YAML().dump(data, file)
 
 
 class ConfigPlugin(OPSICLIPlugin):
