eeeb0161b2814921d2944416c75341f1b08e3f21
doerrer <n.doerrer@uib.de>
2023-04-19 17:10:55 +0200

    [new,pub] Added support client-logs to fetch archive of logfiles from client

--- plugins/support/python/__init__.py
+++ plugins/support/python/__init__.py
@@ -2,11 +2,13 @@
 This command can be used to identify potential problems in an opsi environment
 """
 
+from pathlib import Path
+
 import rich_click as click  # type: ignore[import]
 from opsicommon.logging import get_logger  # type: ignore[import]
 
-from opsicli.io import Attribute, Metadata
-from opsicli.io import write_output
+from opsicli.io import Attribute, Metadata, write_output
+from opsicli.opsiservice import get_service_connection
 from opsicli.plugin import OPSICLIPlugin
 
 from .worker import default_health_check
@@ -25,9 +27,11 @@ def cli() -> None:  # The docstring is used in opsi-cli support --help
 	logger.trace("support command")
 
 
-@cli.command()
+@cli.command(short_help="Print server health check")
 def health_check() -> None:
-
+	"""
+	This command triggers health checks on the server and prints output.
+	"""
 	metadata = Metadata(
 		attributes=[
 			Attribute(id="id", description=""),
@@ -44,10 +48,23 @@ def health_check() -> None:
 	write_output(data=data, metadata=metadata)
 
 
+@cli.command(short_help="Get logfile archive from client")
+@click.argument("client", type=str)
+@click.option("--path", help="Path to put log archive", type=click.Path(file_okay=False, dir_okay=True, path_type=Path), default=Path("."))
+def client_logs(client: str, path: Path) -> None:
+	"""
+	This command instructs an opsi client to pack its logs and deliver them as an archive.
+	"""
+	service = get_service_connection()
+	# instruct client via messagebus to upload its logs and get id
+	# request file with id at opsiconfd
+	# put file at destination path and print name to console
+
+
 # This class keeps track of the plugins meta-information
-class CustomPlugin(OPSICLIPlugin):
+class SupportPlugin(OPSICLIPlugin):
 	name: str = "Support"
 	description: str = __description__
 	version: str = __version__
 	cli = cli
-	flags: list[str] = []
+	flags: list[str] = ["protected"]
