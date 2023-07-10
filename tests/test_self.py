501bdd576f318790a4bc7dc397445925de1d1078
Jan Schneider <j.schneider@uib.de>
2023-07-10 17:32:39 +0200

    [fix] Fix test

--- tests/test_self.py
+++ tests/test_self.py
@@ -18,7 +18,7 @@ def test_self_install() -> None:
 		conffile = Path(tempdir) / "conffile.conf"
 		config.config_file_user = conffile
 		binary_path = Path(tempdir) / "out_binary"
-		(exit_code, output) = run_cli(["self", "install", f"--binary-path={binary_path}", "--no-add-to-path"])
+		(exit_code, output) = run_cli(["self", "install", f"--binary-path={binary_path}", "--no-add-to-path", "--no-system"])
 		print(output)
 		assert exit_code == 0
 		assert binary_path.exists()
@@ -30,7 +30,7 @@ def test_self_uninstall() -> None:
 		conffile = Path(tempdir) / "conffile.conf"
 		config.config_file_user = conffile
 		binary_path = Path(tempdir) / "out_binary"
-		(exit_code, _) = run_cli(["self", "install", f"--binary-path={binary_path}", "--no-add-to-path"])
+		(exit_code, _) = run_cli(["self", "install", f"--binary-path={binary_path}", "--no-add-to-path", "--no-system"])
 		(exit_code, output) = run_cli(["self", "uninstall", f"--binary-path={binary_path}"])
 		print(output)
 		assert exit_code == 0
