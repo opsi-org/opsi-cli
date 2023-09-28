6992468a2d182dab41d0a6e3239c499020c7dc12
doerrer <n.doerrer@uib.de>
2023-09-28 16:59:17 +0200

    [chg,pub] refactored and extended support health-check

--- plugins/support/python/worker.py
+++ plugins/support/python/worker.py
@@ -7,26 +7,47 @@ from typing import Any
 from opsicli.opsiservice import get_service_connection
 
 
-def default_health_check() -> list[dict[str, Any]]:
-	service_data = get_service_connection().jsonrpc("service_healthCheck")
+def status_color_id(entry: dict[str, Any]) -> str:
+	if entry["check_status"] == "ok":
+		return f"[green]{entry['check_id']}[/green]"
+	if entry["check_status"] == "warning":
+		return f"[yellow]{entry['check_id']}[/yellow]"
+	return f"[red]{entry['check_id']}[/red]"
 
+
+def category_health_check(category: str) -> list[dict[str, Any]]:
+	service_data = get_service_connection().jsonrpc("service_healthCheck")
 	data = []
 	for entry in service_data:
-		data.append(
-			{
-				"id": entry["check_id"],
-				"status": entry["check_status"],
-				"message": entry["message"],
-				"err_status": entry["check_status"],
-			}
-		)
-		for result in entry["partial_results"]:
-			data.append(
-				{
-					"partial_results": result["check_id"],
-					"partial_results_status": result["check_status"],
-					"partial_results_message": result["message"],
-					"err_status": result["check_status"],
-				}
-			)
+		if not entry["check_id"] == category:
+			continue
+		data.append({"id": status_color_id(entry), "details": f"[bold white]{entry['message']}[/bold white]"})
+		for partial in entry["partial_results"]:
+			entry_dict = {"id": status_color_id(partial)}
+			entry_dict["details"] = partial["message"]
+			data.append(entry_dict)
+		break
+	return data
+
+
+def default_health_check(detailed: bool = False) -> list[dict[str, Any]]:
+	service_data = get_service_connection().jsonrpc("service_healthCheck")
+	data = []
+	for entry in service_data:
+		entry_dict = {"id": status_color_id(entry)}
+		problems = []
+		if detailed:
+			for partial in entry["partial_results"]:
+				if partial["check_status"] == "warning":
+					problems.append(f"[yellow]{partial['message']}[/yellow]")
+				if partial["check_status"] == "error":
+					problems.append(f"[red]{partial['message']}[/red]")
+			if problems:
+				entry_dict["details"] = "\n".join(problems)
+			else:
+				entry_dict["details"] = "[green]No problems detected[/green]"
+		else:
+			entry_dict["details"] = entry["message"]
+		data.append(entry_dict)
+
 	return data
