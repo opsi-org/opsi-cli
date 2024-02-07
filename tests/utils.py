238ae08f44db74992fb0ff019da8524870ba62bc
doerrer <n.doerrer@uib.de>
2024-02-07 16:13:07 +0100

    [fix] added setupScript property to temp products

--- tests/utils.py
+++ tests/utils.py
@@ -38,7 +38,13 @@ def tmp_client(service: ServiceClient, name: str) -> Generator[None, None, None]
 @contextmanager
 def tmp_product(service: ServiceClient, name: str) -> Generator[None, None, None]:
 	try:
-		product_dict = {"id": name, "type": "LocalbootProduct", "productVersion": "1", "packageVersion": "1"}
+		product_dict = {
+			"id": name,
+			"type": "LocalbootProduct",
+			"productVersion": "1",
+			"packageVersion": "1",
+			"setupScript": "setup.opsiscript",
+		}
 		depot_id = service.jsonrpc("host_getObjects", [[], {"type": "OpsiConfigserver"}])[0]["id"]
 		service.jsonrpc("product_createObjects", params=[product_dict])
 		pod_dict = {"productId": name, "depotId": depot_id, "productType": "LocalbootProduct", "productVersion": "1", "packageVersion": "1"}
