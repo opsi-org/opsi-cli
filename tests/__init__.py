290682b04165eba9348fad2b2d9fa1ea38c24a39
Jeena John <j.john@uib.de>
2024-02-29 17:10:29 +0100

    [chg] update test setup

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,5 @@ opsi-cli tests (general test setup)
 import os
 
 OPSI_HOSTNAME = os.environ.get("OPSI_HOST", "localhost")
+OPSI_USERNAME = os.environ.get("OPSI_USERNAME", "adminuser")
+OPSI_PASSWORD = os.environ.get("OPSI_PASSWORD", "vhahd8usaz")
