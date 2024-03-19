ab1123016a2013f25b29f106efce98e5bc39dae1
doerrer <n.doerrer@uib.de>
2024-03-19 14:32:10 +0100

    [fix] fixed test_depot for the case of unlicensed vpn

--- tests/test_depot.py
+++ tests/test_depot.py
@@ -13,12 +13,15 @@ from .utils import (
 
 
 @pytest.mark.requires_testcontainer
-def test_docker_execute() -> None:
+def test_depot_execute() -> None:
 	with container_connection():
 		connection = get_service_connection()
 		configserver = connection.jsonrpc("host_getObjects", params=[[], {"type": "OpsiConfigserver"}])[0]["id"]
 		cmd = ["depot", "--depots", configserver, "execute", "whoami"]
 		exit_code, output, _stderr = run_cli(cmd)
 		print(output)
-		assert exit_code == 0
-		assert f"service:depot:{configserver}:process: opsiconfd" in output
+		assert exit_code in (0, 1)
+		if exit_code == 0:  # necessary module is licensed
+			assert f"service:depot:{configserver}:process: opsiconfd" in output
+		else:  # necessary module is not licensed
+			assert f"service:depot:{configserver}:process: Failed to start process" in output
