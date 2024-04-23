3e2ed47df9fa8918cd27ffcd045039b658238955
Jan Schneider <j.schneider@uib.de>
2024-04-23 18:51:47 +0200

    [fix] Fix test

--- tests/test_depot.py
+++ tests/test_depot.py
@@ -18,10 +18,11 @@ def test_depot_execute() -> None:
 		connection = get_service_connection()
 		configserver = connection.jsonrpc("host_getObjects", params=[[], {"type": "OpsiConfigserver"}])[0].id
 		cmd = ["depot", "--depots", configserver, "execute", "whoami"]
-		exit_code, output, _stderr = run_cli(cmd)
-		print(output)
+		exit_code, stdout, stderr = run_cli(cmd)
+		print(stderr)
+		print(stdout)
 		assert exit_code in (0, 1)
 		if exit_code == 0:  # necessary module is licensed
-			assert f"service:depot:{configserver}:process" in output and "opsiconfd" in output
+			assert f"service:depot:{configserver}:process" in stdout and "opsiconfd" in stdout
 		else:  # necessary module is not licensed
-			assert f"service:depot:{configserver}:process" in output and "Failed to start process" in output
+			assert f"service:depot:{configserver}:process" in stderr and "Failed to start process" in stderr
