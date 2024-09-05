6d4d69a0a4ce6fb6d1a47a7263e1d55362367117
Jeena John <j.john@uib.de>
2024-09-05 16:44:49 +0200

    [new] Add progress listener for package extraction

---
+++ plugins/package/python/package_progress.py
@@ -0,0 +1,19 @@
+"""
+This module provides a progress listener for various package operations using the rich progress bar.
+"""
+
+from opsicommon.package.archive import ArchiveProgress, ArchiveProgressListener
+from rich.progress import Progress
+
+
+class PackageProgressListener(ArchiveProgressListener):
+	def __init__(self, progress: Progress, task_message: str):
+		self.progress = progress
+		self.started = False
+		self.task_id = self.progress.add_task(task_message, total=None)
+
+	def progress_changed(self, progress: ArchiveProgress) -> None:
+		if not self.started:
+			self.started = True
+			self.progress.tasks[self.task_id].total = 100
+		self.progress.update(self.task_id, completed=progress.percent_completed)
