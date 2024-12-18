2b00ae9461729523434fa90c824195a70b4dba7c
doerrer <n.doerrer@uib.de>
2024-12-18 17:00:09 +0100

    [new] added test for breaking messagebus connection

--- tests/test_messagebus.py
+++ tests/test_messagebus.py
@@ -8,7 +8,7 @@ from threading import Thread
 import pytest
 from opsicommon.client.opsiservice import ServiceClient
 
-from opsicli.messagebus import JSONRPCMessagebusConnection
+from opsicli.messagebus import JSONRPCMessagebusConnection, MessagebusConnection
 from opsicli.opsiservice import get_service_connection
 
 from .utils import container_connection, run_cli
@@ -62,21 +62,23 @@ def test_messagebus_jsonrpc_multiple() -> None:
 
 @pytest.mark.requires_testcontainer
 def test_wait_for_event() -> None:
+	LISTENER_SETUP_WAIT = 2.0  # Waiting to make sure listener is set up
+	LISTENING_TIMEOUT = 4.0  # Time to wait for the event to occur
+
 	class CreateHostThread(Thread):
 		def __init__(self, client: ServiceClient) -> None:
 			super().__init__(daemon=True)
 			self.client = client
 
 		def run(self) -> None:
-			time.sleep(4.0)  # Resetting dropped connection: localhost - Waiting 5 seconds before reconnect
+			time.sleep(LISTENER_SETUP_WAIT)
 			self.client.jsonrpc("host_createOpsiClient", params=["dummy.test.tld"])
 
 	with container_connection():
 		service_connection = get_service_connection(recreate=True)
 		cht = CreateHostThread(service_connection)
 		cht.start()
-		# with tmp_client(connection, CLIENT1):
-		cmd = ["-l", "7", "messagebus", "wait-for-event", "host_created", "--timeout", "4"]
+		cmd = ["-l7", "messagebus", "wait-for-event", "host_created", "--timeout", str(LISTENING_TIMEOUT)]
 		exit_code, _stdout, _stderr = run_cli(cmd)
 		cht.join()
 		service_connection.jsonrpc("host_delete", params=["dummy.test.tld"])
@@ -89,21 +91,32 @@ def test_wait_for_event() -> None:
 
 @pytest.mark.requires_testcontainer
 def test_wait_for_event_data() -> None:
+	LISTENER_SETUP_WAIT = 2.0  # Waiting to make sure listener is set up
+	LISTENING_TIMEOUT = 4.0  # Time to wait for the event to occur
+
 	class CreateHostThread(Thread):
 		def __init__(self, client: ServiceClient) -> None:
 			super().__init__(daemon=True)
 			self.client = client
 
 		def run(self) -> None:
-			time.sleep(4.0)
+			time.sleep(LISTENER_SETUP_WAIT)
 			self.client.jsonrpc("host_createOpsiClient", params=["dummy.test.tld"])
 
 	with container_connection():
 		service_connection = get_service_connection(recreate=True)
 		cht = CreateHostThread(service_connection)
 		cht.start()
-		# with tmp_client(connection, CLIENT1):
-		cmd = ["-l", "7", "messagebus", "wait-for-event", "host_created", "--data", "id=dummy.test.tld", "--timeout", "4"]
+		cmd = [
+			"-l7",
+			"messagebus",
+			"wait-for-event",
+			"host_created",
+			"--data",
+			"id=dummy.test.tld",
+			"--timeout",
+			str(LISTENING_TIMEOUT),
+		]
 		exit_code, _stdout, _stderr = run_cli(cmd)
 		cht.join()
 		service_connection.jsonrpc("host_delete", params=["dummy.test.tld"])
@@ -111,9 +124,49 @@ def test_wait_for_event_data() -> None:
 
 		cht = CreateHostThread(service_connection)
 		cht.start()
-		# with tmp_client(connection, CLIENT1):
-		cmd = ["-l", "7", "messagebus", "wait-for-event", "host_created", "--data", "id=some.other.host", "--timeout", "4"]
+		cmd = [
+			"-l7",
+			"messagebus",
+			"wait-for-event",
+			"host_created",
+			"--data",
+			"id=some.other.host",
+			"--timeout",
+			str(LISTENING_TIMEOUT),
+		]
 		exit_code, _stdout, _stderr = run_cli(cmd)
 		cht.join()
 		service_connection.jsonrpc("host_delete", params=["dummy.test.tld"])
 		assert exit_code == 1  # timeout reached
+
+
+@pytest.mark.requires_testcontainer
+def test_messagebus_reconnect_problem() -> None:
+	RECONNECT_WAIT = 5.0  # Resetting dropped connection: localhost - Waiting 5 seconds before reconnect
+	LISTENER_SETUP_WAIT = 2.0  # Waiting to make sure listener is set up
+	LISTENING_TIMEOUT = 4.0  # Time to wait for the event to occur
+
+	class CreateHostThread(Thread):
+		def __init__(self, client: ServiceClient) -> None:
+			super().__init__(daemon=True)
+			self.client = client
+
+		def run(self) -> None:
+			time.sleep(RECONNECT_WAIT + LISTENER_SETUP_WAIT)
+			print(self.client.jsonrpc("host_createOpsiClient", params=["dummy.test.tld"]))
+
+	with container_connection():
+		connection = MessagebusConnection()
+		# without the following instruction, the connection will not have to be reset and there is no need for RECONNECT_WAIT
+		with connection.connection():
+			pass
+
+		# with recreate = True, there is no need for RECONNECT_WAIT
+		service_client = get_service_connection(recreate=False)
+		cht = CreateHostThread(service_client)
+		cht.start()
+		cmd = ["-l", "7", "messagebus", "wait-for-event", "host_created", "--timeout", str(LISTENING_TIMEOUT)]
+		exit_code, _stdout, _stderr = run_cli(cmd)
+		cht.join()
+		service_client.jsonrpc("host_delete", params=["dummy.test.tld"])
+		assert exit_code == 0  # 'host_created' event found
