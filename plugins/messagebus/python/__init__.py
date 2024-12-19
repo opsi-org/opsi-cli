aced687742b5a93aa2e25116954a8638ed1cc8f7
doerrer <n.doerrer@uib.de>
2024-12-19 15:28:19 +0100

    [new,pub] Added new command messagebus wait-for-installation

--- plugins/messagebus/python/__init__.py
+++ plugins/messagebus/python/__init__.py
@@ -4,6 +4,7 @@ opsi-cli basic command line interface for opsi
 messagebus plugin
 """
 
+import sys
 from threading import Event
 from typing import Any
 
@@ -24,7 +25,7 @@ class WaitForEventMessagebusConnection(MessagebusConnection):
 	def __init__(self) -> None:
 		MessagebusConnection.__init__(self)
 		self.wait_for_type: str | None = None
-		self.wait_for_data: dict[str, Any] | None = None
+		self.wait_for_data: list[dict[str, Any]] | None = None
 		self.event_found_event = Event()  # why does everything have be named event?
 		self.result: EventMessage | None = None
 
@@ -33,15 +34,24 @@ class WaitForEventMessagebusConnection(MessagebusConnection):
 			logger.debug("Found event of different type %s", message.event)
 			return
 		if self.wait_for_data:
-			for key, value in self.wait_for_data.items():
-				if message.data.get(key) != value:
-					logger.debug("Found event with different data %s", message.data)
+			for data in self.wait_for_data:
+				for key, value in data.items():
+					if message.data.get(key) != value:
+						break
+				else:  # all key-value pairs matched
+					logger.notice("Received requested event %s (data=%s)", message, message.data)
+					self.result = message
+					self.event_found_event.set()
 					return
-		logger.notice("Received requested event %s (data=%s)", message, message.data)
-		self.result = message
-		self.event_found_event.set()
+			logger.debug("Found event with different data %s", message.data)
+		else:
+			logger.notice("Received requested event %s (data=%s)", message, message.data)
+			self.result = message
+			self.event_found_event.set()
 
-	def wait_for_event(self, type: str | None = None, data: dict[str, Any] | None = None, timeout: float | None = None) -> EventMessage:
+	def wait_for_event(
+		self, type: str | None = None, data: list[dict[str, Any]] | None = None, timeout: float | None = None
+	) -> EventMessage:
 		self.wait_for_type = type
 		self.wait_for_data = data
 		logger.notice("Waiting for event of type %r with data %s to occur", self.wait_for_type, self.wait_for_data)
@@ -76,10 +86,33 @@ def wait_for_event(type: str, data: list[str], timeout: float | None) -> None:
 	opsi-cli messagebus wait-for-event command
 	"""
 	mbus_connection = WaitForEventMessagebusConnection()
-	data_dict = None
+	data_list: list[dict[str, Any]] | None = None
 	if data:
-		data_dict = {entry[0].strip(): entry[1].strip() for entry in [assignment.split("=", 1) for assignment in data]}
-	mbus_connection.wait_for_event(type=type, data=data_dict, timeout=timeout)
+		data_list = [{entry[0].strip(): entry[1].strip() for entry in [assignment.split("=", 1) for assignment in data]}]
+	result = mbus_connection.wait_for_event(type=type, data=data_list, timeout=timeout)
+	print(result.data)
+
+
+@cli.command(name="wait-for-installation", short_help="Wait for a specific event on the messagebus")
+@click.argument("client", type=str)
+@click.argument("product", type=str)
+@click.argument("action-request", type=str)
+@click.option("--timeout", help="Timeout in seconds", type=float, default=None)
+def wait_for_installation(client: str, product: str, action_request: str, timeout: float | None) -> None:
+	"""
+	opsi-cli messagebus wait-for-installation command
+	"""
+	mbus_connection = WaitForEventMessagebusConnection()
+	wanted_status = "not_installed" if action_request == "uninstall" else "installed"
+	wait_for_data = [
+		{"clientId": client, "productId": product, "actionRequest": "none", "installationStatus": wanted_status},
+		{"clientId": client, "productId": product, "actionRequest": "none", "installationStatus": "unknown"},
+	]
+	result = mbus_connection.wait_for_event(type="productOnClient_updated", data=wait_for_data, timeout=timeout)
+	print(result.data)
+	if result.data.get("installation_status") == "unknown":
+		logger.error("Installation failed")
+		sys.exit(1)
 
 
 # This class keeps track of the plugins meta-information
