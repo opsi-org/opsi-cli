74d506be706ff99fa20e1c1fb82f2a9f9472ae3a
Jeena John <j.john@uib.de>
2025-06-11 09:59:11 +0200

    [chg] update tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -48,7 +48,7 @@ def test_handle_list_attributes() -> None:
 
 def test_dry_run_handling() -> None:
 	# Test that the help output includes the dry-run support note for a dry-run capable command
-	exit_code, stdout, stderr = run_cli(["self", "upgrade", "--help"])
+	exit_code, stdout, stderr = run_cli(["self", "install", "--help"])
 	combined_output = stdout + stderr
 	assert "This command supports --dry-run: actions will be simulated and not performed." in combined_output
 
@@ -57,7 +57,7 @@ def test_dry_run_handling() -> None:
 		[
 			"--dry-run",
 			"self",
-			"upgrade",
+			"install",
 		]
 	)
 	stderr = " ".join(stderr.split())
