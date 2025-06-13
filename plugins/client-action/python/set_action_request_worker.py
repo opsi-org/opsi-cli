3e90389b4c7e5ca5bb90e97ec1b6126d726e237e
Jan Schneider <j.schneider@uib.de>
2025-06-13 12:26:07 +0200

    [chg] Do not select actionProgress, actionResult and installationStatus attributes by default

--- plugins/client-action/python/set_action_request_worker.py
+++ plugins/client-action/python/set_action_request_worker.py
@@ -382,9 +382,9 @@ class SetActionRequestWorker(ClientActionWorker):
 				Attribute(id="clientId", description="ID of the client", identifier=True, data_type="str"),
 				Attribute(id="productId", description="ID of the product", identifier=True, data_type="str"),
 				Attribute(id="actionRequest", description="Product action request set", data_type="str"),
-				Attribute(id="actionProgress", description="Product action progress", data_type="str"),
-				Attribute(id="actionResult", description="Product action result", data_type="str"),
-				Attribute(id="installationStatus", description="Product installation status", data_type="str"),
+				Attribute(id="actionProgress", description="Product action progress", data_type="str", selected=False),
+				Attribute(id="actionResult", description="Product action result", data_type="str", selected=False),
+				Attribute(id="installationStatus", description="Product installation status", data_type="str", selected=False),
 			]
 		)
 		write_output(
