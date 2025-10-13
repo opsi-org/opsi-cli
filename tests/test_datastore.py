7fb000f945cfd8a5c0c14d1417545249f682831a
erdelt <j.erdelt@uib.de>
2025-10-13 15:10:03 +0200

    [add] added test for comma seperated objectId's and comma seperated objectId's with wildcards.

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -5,6 +5,13 @@ from .utils import run_cli, tmp_client
 
 CLIENT_ID_1 = "pytest-client1.test.tld"
 CLIENT_ID_2 = "pytest-client2.test.tld"
+CLIENT_ID_3 = "client3.test.tld"
+CLIENT_ID_4 = "client4.test.tld"
+CLIENT_ID_WILDCARD_1 = "py*"
+CLIENT_ID_WILDCARD_2 = "cli*"
+CLIENT_ID_WILDCARD_3 = "pyt*"
+CLIENT_ID_WILDCARD_4 = "clie*"
+
 DEPOT_ID = "opsi.opsi.test"
 
 CONFIG_ID = "opsi.check.enabled"
@@ -33,6 +40,8 @@ def test_config_state_list(admin_service_client: ServiceClient) -> None:
 	with (
 		tmp_client(admin_service_client, CLIENT_ID_1),
 		tmp_client(admin_service_client, CLIENT_ID_2),
+		tmp_client(admin_service_client, CLIENT_ID_3),
+		tmp_client(admin_service_client, CLIENT_ID_4),
 	):
 		all_configs = admin_service_client.jsonrpc("config_getObjects", params=[])
 
@@ -50,28 +59,29 @@ def test_config_state_list(admin_service_client: ServiceClient) -> None:
 			["--output-format", "csv", "datastore", "config-state", "list", "--object-id", f"{CLIENT_ID_1}"]
 		)
 		assert exit_code == 0
-		stdout_list = stdout_into_list(_stdout)
 		print(_stdout)
-		print("-" * 80)
+		stdout_list = stdout_into_list(_stdout)
 		for element in stdout_list[1:]:
 			print(element[0])
 			assert element[0] == CLIENT_ID_1
 		# test if all configs are shown in output table
 		assert len(stdout_list) - 1 == len(all_configs)
 
-		# All objectId's (2), one configId
+		# All objectId's (4), one configId
 		exit_code, _stdout, _stderr = run_cli(
-			["--output-format", "csv", "datastore", "config-state", "list", "--config-id", f"{CONFIG_ID}"]
+			["--output-format", "csv", "--sort-by", "objectId", "datastore", "config-state", "list", "--config-id", f"{CONFIG_ID}"]
 		)
 		assert exit_code == 0
-		assert stdout_into_list(_stdout)[1][0] == CLIENT_ID_1
-		assert stdout_into_list(_stdout)[2][0] == CLIENT_ID_2
-		assert len(stdout_into_list(_stdout)) - 1 == 2
+		assert stdout_into_list(_stdout)[1][0] == CLIENT_ID_3
+		assert stdout_into_list(_stdout)[2][0] == CLIENT_ID_4
+		assert stdout_into_list(_stdout)[3][0] == CLIENT_ID_1
+		assert stdout_into_list(_stdout)[4][0] == CLIENT_ID_2
+		assert len(stdout_into_list(_stdout)) - 1 == 4
 
 		# All objectId's (2), all configId's
 		exit_code, _stdout, _stderr = run_cli(["--output-format", "csv", "datastore", "config-state", "list"])
 		assert exit_code == 0
-		assert len(stdout_into_list(_stdout)) - 1 == 2 * len(all_configs)
+		assert len(stdout_into_list(_stdout)) - 1 == 4 * len(all_configs)
 
 		# (SERVER)
 		# test if the origin and changed value of a config is shown correctly
@@ -159,3 +169,89 @@ def test_config_state_list(admin_service_client: ServiceClient) -> None:
 			stdout_into_list(_stdout)[1][2] == "0"
 		)  # only second row is of interest, first row of stdout_list[0][i]=([clientId, configId, value, origin])
 		assert stdout_into_list(_stdout)[1][3] == "[blue]client[/blue]"
+
+		# test comma seperated objectId's with wildcards
+		exit_code, _stdout, _stderr = run_cli(
+			[
+				"--output-format",
+				"csv",
+				"--sort-by",
+				"objectId",
+				"datastore",
+				"config-state",
+				"list",
+				"--object-id",
+				f"{CLIENT_ID_3},{CLIENT_ID_4}",
+				"--config-id",
+				f"{CONFIG_ID}",
+			]
+		)
+		print(_stdout)
+		assert exit_code == 0
+		assert stdout_into_list(_stdout)[1][0] == CLIENT_ID_3
+		assert stdout_into_list(_stdout)[2][0] == CLIENT_ID_4
+		assert len(stdout_into_list(_stdout)) - 1 == 2
+
+		exit_code, _stdout, _stderr = run_cli(
+			[
+				"--output-format",
+				"csv",
+				"--sort-by",
+				"objectId",
+				"datastore",
+				"config-state",
+				"list",
+				"--object-id",
+				f"{CLIENT_ID_WILDCARD_1},{CLIENT_ID_WILDCARD_2}",
+				"--config-id",
+				f"{CONFIG_ID}",
+			]
+		)
+		assert exit_code == 0
+		assert stdout_into_list(_stdout)[1][0] == CLIENT_ID_3
+		assert stdout_into_list(_stdout)[2][0] == CLIENT_ID_4
+		assert stdout_into_list(_stdout)[3][0] == CLIENT_ID_1
+		assert stdout_into_list(_stdout)[4][0] == CLIENT_ID_2
+		assert len(stdout_into_list(_stdout)) - 1 == 4
+
+		exit_code, _stdout, _stderr = run_cli(
+			[
+				"--output-format",
+				"csv",
+				"--sort-by",
+				"objectId",
+				"datastore",
+				"config-state",
+				"list",
+				"--object-id",
+				f"{CLIENT_ID_WILDCARD_1},{CLIENT_ID_WILDCARD_3}",
+				"--config-id",
+				f"{CONFIG_ID}",
+			]
+		)
+		assert exit_code == 0
+		assert stdout_into_list(_stdout)[1][0] == CLIENT_ID_1
+		assert stdout_into_list(_stdout)[2][0] == CLIENT_ID_2
+		assert len(stdout_into_list(_stdout)) - 1 == 2
+
+		exit_code, _stdout, _stderr = run_cli(
+			[
+				"--output-format",
+				"csv",
+				"--sort-by",
+				"objectId",
+				"datastore",
+				"config-state",
+				"list",
+				"--object-id",
+				f"{CLIENT_ID_WILDCARD_1},{CLIENT_ID_WILDCARD_2},{CLIENT_ID_WILDCARD_3},{CLIENT_ID_WILDCARD_4}",
+				"--config-id",
+				f"{CONFIG_ID}",
+			]
+		)
+		assert exit_code == 0
+		assert stdout_into_list(_stdout)[1][0] == CLIENT_ID_3
+		assert stdout_into_list(_stdout)[2][0] == CLIENT_ID_4
+		assert stdout_into_list(_stdout)[3][0] == CLIENT_ID_1
+		assert stdout_into_list(_stdout)[4][0] == CLIENT_ID_2
+		assert len(stdout_into_list(_stdout)) - 1 == 4
