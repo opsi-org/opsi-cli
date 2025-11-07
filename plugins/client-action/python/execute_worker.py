9eb7b6097842134c5625622048c38167a0275ce5
doerrer <n.doerrer@uib.de>
2025-11-07 14:05:37 +0100

    [fix] fixed typing

--- plugins/client-action/python/execute_worker.py
+++ plugins/client-action/python/execute_worker.py
@@ -87,14 +87,14 @@ class ExecuteWorker(ClientActionWorker):
 
 				if isinstance(result, Exception):
 					logger.error("Exception occured while executing opsiscript on %s: %s", channel, result)
-					console_print(line_prefix + Text(str(result), style="red" if config.color else None), output_type=OutputType.DATA)
+					console_print(line_prefix + Text(str(result), style="red" if config.color else ""), output_type=OutputType.DATA)
 					highest_exit_code = max(highest_exit_code, 1)
 					continue
 
 				if "error" in result:
 					error_message = result["error"].get("message", "Unknown error")
 					logger.error("Error occurred while executing opsiscript on %s: %s", channel, error_message)
-					console_print(line_prefix + Text(error_message, style="red" if config.color else None), output_type=OutputType.DATA)
+					console_print(line_prefix + Text(error_message, style="red" if config.color else ""), output_type=OutputType.DATA)
 					highest_exit_code = max(highest_exit_code, result["error"].get("code", 1))
 					continue
 
