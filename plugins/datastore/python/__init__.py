46a3c23099c4100da068b1f66b29de14f91359e6
erdelt <j.erdelt@uib.de>
2026-03-18 10:15:43 +0100

    [chg] Improved helptext

--- plugins/datastore/python/__init__.py
+++ plugins/datastore/python/__init__.py
@@ -186,15 +186,17 @@ def list_config_state(object_ids: str, config_ids: str) -> None:
 # ========================================================CONFIG-STATE SET========================================================
 @config_state.command(
 	name="set",
-	short_help="Update an existing config state or create a new one if it doesn't exist. Using 'all' as the object ID will apply the value to all objects.",
+	short_help="Change the value(s) of a config-state for one or multiple clients.",
 )
 @click.argument("object-id", type=str)
 @click.argument("config-id", type=str)
-@click.option("--values", type=str, required=True)
-@click.option("--sep", type=str, required=False, default=",")
+@click.option("--values", type=str, required=True, help="New value(s), separated by commas.")
+@click.option("--sep", type=str, required=False, default=",", help="Choose your custom delimiter.")
 def set_config_state_value(config_id: str, object_id: str, values: str, sep=str) -> None:
 	"""
-	Change values of config states.
+	This command updates an existing config-state or creates a new one if it doesn't exist.
+	For object ID use commas as separators and 'all' to include all IDs. Wildcards (*) are supported."
+
 	"""
 
 	def set_bool_config(object_ids: list[str], config_id: str, value: str) -> None:
