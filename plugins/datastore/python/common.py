b504d29418bd70cc05ee04f4e58bf2e9cf25b142
erdelt <j.erdelt@uib.de>
2026-04-07 14:33:47 +0200

    [fix] Linting

--- plugins/datastore/python/common.py
+++ plugins/datastore/python/common.py
@@ -215,8 +215,9 @@ def validate_against_possible_values(val: str, obj: Any) -> list[str] | list[boo
 	return []
 
 
-def filter_by_attributes(data: list[dict[str, Any]], filter: dict[str, str], attributes: list[Attribute]) -> list[dict[str, Any]]:
-
+def filter_by_attributes(data: list[dict[str, Any]], filter: dict[str, str], attributes: list[Attribute] | None) -> list[dict[str, Any]]:
+	if attributes is None:
+		raise RuntimeError("Filter_by_attributes requires a list of attributes.")
 	available_attributes = {attr.id: attr for attr in attributes}
 	filtered_data = []
 	for entry in data:
