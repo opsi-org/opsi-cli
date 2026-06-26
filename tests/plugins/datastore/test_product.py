c6d8228d05336706916bd5b3c2082ad9cf416ced
erdelt <j.erdelt@uib.de>
2026-06-26 14:50:35 +0200

    [fix] Determine depotId in expected_error

--- tests/plugins/datastore/test_product.py
+++ tests/plugins/datastore/test_product.py
@@ -10,8 +10,11 @@ from unittest.mock import MagicMock, patch
 import pytest
 from opsi.opsi.service.client import ServiceClient
 
+<<<<<<< HEAD
 from plugins.datastore.python.product_client_state import list_product_client_state
 from tests.conftest import admin_service_client
+=======
+>>>>>>> [fix] Determine depotId in expected_error
 from tests.utils import get_depot_id, run_cli, stdout_into_list, tmp_client, tmp_product
 
 CLIENT_ID_1 = "pytest-client1.test.tld"
@@ -20,8 +23,11 @@ CLIENT_ID_2 = "pytest-client2.test.tld"
 PRODUCT_ID_1 = "pytest-product1"
 PRODUCT_ID_2 = "pytest-product2"
 
+<<<<<<< HEAD
 DEPOT_ID = get_depot_id(admin_service_client())
 
+=======
+>>>>>>> [fix] Determine depotId in expected_error
 # ===============
 # HELP FUNCTIONS
 # ===============
@@ -136,7 +142,7 @@ class TestProductUnlock:
 					"The specified depotId was not found.",
 					"Please use one or multiple of the available depotId's",
 					"Available depotId's are:",
-					f"{DEPOT_ID}",
+					f"{get_depot_id(admin_service_client)}",
 				],
 			)
 
