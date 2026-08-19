f0b245787b9384ce347a53ebce63e65138070712
erdelt <j.erdelt@uib.de>
2026-08-19 12:13:31 +0200

    [fix] Fix pytest

--- tests/plugins/datastore/test_config_state.py
+++ tests/plugins/datastore/test_config_state.py
@@ -780,7 +780,7 @@ def test_config_state_list(
 			],
 			None,
 			None,
-			("Multiple values `bonidepot.uib.local, bonifax.uib.local`", "are not allowed for the given UnicodeConfig:"),
+			("Multiple values `bonidepot.uib.local, bonifax.uib.local`", "are not allowed", "UnicodeConfig:"),
 		),
 		# Missing configId
 		(
